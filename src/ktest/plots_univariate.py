e45897c135ea77f13931710e435b16bf17fa7bd8
AnthoOzier <anthony.ozier-lafontaine@eleves.ec-nantes.fr>
2022-10-24 09:31:42 +0200

    correlations and plot univariate
    Nouvelles fonctions dans utils_reversion (qui finalement ne servent pas):
    get_dict_testers_outliers_vs_each_condition, get_dict_testers_condition_vs_each_other_condition, get_tester_outliers_vs_all, 
    
    univariate_testing.py : 
    new : add_log2fc_to_var (not finished yet)
    
    plot_univariate.py : 
    update : volcano_plot 
    functions that link univariate and multivariate testing : plot_pval_of_correlated_genes, plot_correlation_of_DEgenes
    
    correlation_operations.py : 
    create class and update functions : 
    compute_corr_proj_var, find_correlated_variables, get_corr_of_variable
    
    base.py : 
    new : get_corr_name

--- src/ktest/plots_univariate.py
+++ src/ktest/plots_univariate.py
@@ -121,7 +121,6 @@ class Plot_Univariate(TruncationSelection,Plot_Summarized,Univariate):
         
         return(fig,axd)
 
-
     def plot_pc_of_expression_univariate(self,variable,trunc,
                                                 color=None,marker=None,highlight=None,
                                                 ):
@@ -130,7 +129,8 @@ class Plot_Univariate(TruncationSelection,Plot_Summarized,Univariate):
         axd = fig.subplot_mosaic("AB\nCD",gridspec_kw=dict(height_ratios=[1, 2],width_ratios=[3,2]),)
         
         # Pval and errors 
-        self.plot_pval_and_errors(fig=fig,ax=axd['B'],truncations_of_interest=[trunc],adjust=False)
+        toi = [trunc] if trunc <=30 else []
+        self.plot_pval_and_errors(t=30,fig=fig,ax=axd['B'],truncations_of_interest=toi,adjust=False)
         
         self.density_proj(t=variable,proj=variable,fig=fig,ax=axd['A'])
         self.scatter_proj(projection=[variable,trunc],xproj=variable,yproj='proj_kpca',yname=self.get_kfdat_name(),
@@ -155,7 +155,6 @@ class Plot_Univariate(TruncationSelection,Plot_Summarized,Univariate):
         fig.tight_layout()
         
         return(fig,axd)
-
 
     def plot_pc_and_discriminant_of_expression_univariate(self,variable,trunc,
                                                 color=None,marker=None,highlight=None,
@@ -204,31 +203,174 @@ class Plot_Univariate(TruncationSelection,Plot_Summarized,Univariate):
         
         return(fig,axd)
    
-   
-   
+    def volcano_plot(self,var_prefix,color=None,exceptions=[],
+                    focus=None,zero_pvals=False,fig=None,ax=None,BH=False,threshold=1,plot_others=False):
+        # quand la stat est trop grande, la fonction chi2 de scipy.stat renvoie une pval nulle
+        # on ne peut pas placer ces gènes dans le volcano plot alors ils ont leur propre graphe
+
+        if fig is None:
+            fig,ax = plt.subplots(figsize=(9,15))
+
+        BH_str = 'BHc' if BH else ''
+        
+        zpval_str = '= 0' if zero_pvals else '>0'
+        var = self.get_var()
+
+        pval_name = f'{var_prefix}_pval{BH_str}' 
+        if BH and pval_name not in var:
+            self.correct_BenjaminiHochberg_pval_univariate(var_prefix=var_prefix)
+        pval = var[pval_name]
+        pval = filter_genes_wrt_pval(pval,exceptions,focus,zero_pvals,threshold)
+        
+        print(f'{var_prefix} ngenes with pvals {BH_str} {zpval_str}: {len(pval)}')
+        
+        genes = []
+        if len(pval) != 0:
+            kfda = var[f'{var_prefix}_kfda']
+            errB = var[f'{var_prefix}_errB']
+            logkfda = np.log(kfda[kfda.index.isin(pval.index)])
+            errB = errB[errB.index.isin(pval.index)]
+
+            zpval_logkfda = np.log(kfda[~kfda.index.isin(pval.index)])
+    #         print(len(logkfda),len(zpval_logkfda))
+
+            xlim = (logkfda.min()-1,logkfda.max()+1)
+    #         xlim = (logkfda.min()-1,zpval_logkfda.max()+1)
+    #             c = self.color_volcano_plot(var_prefix,pval.index,color=color)
+
+            if zero_pvals:
+        #         print('zero')
+                ax.set_title(f'{var_prefix} \ng enes strongly rejected',fontsize=30)
+                ax.set_xlabel(f'log(kfda)',fontsize=20)
+                ax.set_ylabel(f'errB',fontsize=20)
+
+                for g in pval.index.tolist():
+        #             print(g,logkfda[g],errB[g],c[g])
+                    ax.text(logkfda[g],errB[g],g)#,color=c[g])
+                    genes += [g]
+                
+                ax.set_xlim(xlim)
+                ax.set_ylim(0,1)
+
+
+            else:
+        #         print('nz')
+                ax.set_title(f'{var_prefix}\n non zero pvals',fontsize=30)
+                ax.set_xlabel(f'log(kfda)',fontsize=20)
+                ax.set_ylabel(f'-log(pval)',fontsize=20)
+                logpval = -np.log(pval)
+
+
+                for g in pval.sort_values().index.tolist():
+        #             print(g,logkfda[g],logpval[g],c[g])
+                    ax.text(logkfda[g],logpval[g],g)#,color=c[g])
+                    ax.set_xlim(xlim)
+                    ax.set_ylim(0,logpval.max()*1.1)
+                    genes += [g]
+                if plot_others:
+                    for g in zpval_logkfda.index.tolist():
+        #                 print(g)
+                        ax.text(zpval_logkfda[g],logpval.max(),g)
+
+        return(genes,fig,ax)
+    
+    def plot_pval_of_correlated_genes(self,tcorrmax,tpvalmax,proj,prefix,fig=None,axes=None):
+        if fig is None:
+            fig,axes = plt.subplots(ncols=tcorrmax,figsize=(8*tcorrmax,10))
+
+        axes = [axes] if tcorrmax == 1 else axes
+
+
+
+        for tcorr,ax in zip(range(1,tcorrmax+1),axes):
+            corr = self.find_correlated_variables(proj=proj,nvar=0,t=tcorr)
+
+            out = []
+            if proj == 'proj_kfda':
+                print(f'top correlated with DA{tcorr}:',corr.index[:10])
+            if proj == 'proj_kpca':
+                print(f'top correlated with PC{tcorr}:',corr.index[:10])
+                
+            for g in corr.index[::-1]:
+                dout = {}
+                for tpval in range(1,tpvalmax+1):
+                    pval_name = f'{prefix}_{self.get_kfdat_name()}_t{tpval}_pvalBHc'
+                    if pval_name not in self.get_var().columns:
+                        self.correct_BenjaminiHochberg_pval_univariate(var_prefix=pval_name[:-8])
+                    dout[f't{tpval}'] = -np.log(self.get_var()[pval_name][g]+1)/np.log(10)
+                out += [dout]
+
+            dfout = pd.DataFrame(out,index=corr.index)
+    #         print('min',dfout.min().min())
+    #         print('max',dfout.max().max())
+            ax.set_title(f'tcorr={tcorr}',fontsize=30)
+            pcm = ax.pcolor(dfout,vmin=-.3,vmax=0)
+            ax.set_yticks(range(1,len(corr)+1))
+            ax.set_yticklabels(corr.index[::-1])
+            fig.colorbar(pcm, ax=ax, extend='max')
+        fig.tight_layout()
+        return(fig,axes)
+
+    def plot_correlation_of_DEgenes(self,tcorrmax,tpvalmax,proj,prefix,fig=None,axes=None,highlightDE=False):
+        if fig is None:
+            fig,axes = plt.subplots(ncols=tpvalmax,figsize=(8*tpvalmax,10))
+
+        axes = [axes] if tpvalmax == 1 else axes
+
+        for tpval,ax in zip(range(1,tpvalmax+1),axes):
+            pval_name = f'{prefix}_{self.get_kfdat_name()}_t{tpval}_pvalBHc'
+            if pval_name not in self.get_var().columns:
+                self.correct_BenjaminiHochberg_pval_univariate(var_prefix=pval_name[:-8])
+            pval = self.get_var()[pval_name]
+            pval = pval.sort_values()
+
+            out = []
+            print(f'top DE genes for {tpval}:',pval.index[:10])
+                
+            for g in pval.index[::-1]:
+                out += [{f't{tcorr}':self.get_corr_of_variable(proj,g,tcorr) for tcorr in range(1,tcorrmax+1)}]
+
+            dfout = pd.DataFrame(out,index=pval.index)
+    #         print('min',dfout.min().min())
+    #         print('max',dfout.max().max())
+
+            ax.set_title(f'tpval={tpval}',fontsize=30)
+            pcm=ax.pcolor(dfout,vmin=0,vmax=1)
+            if highlightDE:
+                ax.set_yticks(range(len(pval)+1-len(pval[pval<.05]),len(pval)+1))
+                ax.set_yticklabels(pval[pval<.05].index[::-1])
+            else:
+                ax.set_yticks(range(1,len(pval)+1))
+                ax.set_yticklabels(pval.index[::-1])
+            fig.colorbar(pcm, ax=ax, extend='max')
+        fig.tight_layout()
+        plt.show()
+        return(fig,axes)
     # def volcano_plot(self,var_prefix,color=None,exceptions=[],focus=None,zero_pvals=False,fig=None,ax=None,BH=False,threshold=1):
     #     # quand la stat est trop grande, la fonction chi2 de scipy.stat renvoie une pval nulle
     #     # on ne peut pas placer ces gènes dans le volcano plot alors ils ont leur propre graphe
-        
+
     #     if fig is None:
     #         fig,ax = plt.subplots(figsize=(9,15))
+
     #     BH_str = 'BHc' if BH else ''
     #     zpval_str = '= 0' if zero_pvals else '>0'
-        
+    #     dn = self.data_name
+
     #     pval_name = f'{var_prefix}_pval{BH_str}' 
-    #     pval = filter_genes_wrt_pval(self.var[pval_name],exceptions,focus,zero_pvals,threshold)
+    #     pval = filter_genes_wrt_pval(self.var[dn][pval_name],exceptions,focus,zero_pvals,threshold)
     #     print(f'{var_prefix} ngenes with pvals {BH_str} {zpval_str}: {len(pval)}')
     #     genes = []
     #     if len(pval) != 0:
-    #         kfda = self.var[f'{var_prefix}_kfda']
-    #         errB = self.var[f'{var_prefix}_errB']
+    #         kfda = self.var[dn][f'{var_prefix}_kfda']
+    #         errB = self.var[dn][f'{var_prefix}_errB']
     #         kfda = kfda[kfda.index.isin(pval.index)]
     #         errB = errB[errB.index.isin(pval.index)]
 
     #         logkfda = np.log(kfda)
 
     #         xlim = (logkfda.min()-1,logkfda.max()+1)
-    #         c = self.color_volcano_plot(var_prefix,pval.index,color=color)
+    # #             c = self.color_volcano_plot(var_prefix,pval.index,color=color)
 
     #         if zero_pvals:
     #     #         print('zero')
@@ -238,7 +380,7 @@ class Plot_Univariate(TruncationSelection,Plot_Summarized,Univariate):
 
     #             for g in pval.index.tolist():
     #     #             print(g,logkfda[g],errB[g],c[g])
-    #                 ax.text(logkfda[g],errB[g],g,color=c[g])
+    #                 ax.text(logkfda[g],errB[g],g)#,color=c[g])
     #                 ax.set_xlim(xlim)
     #                 ax.set_ylim(0,1)
     #                 genes += [g]
@@ -254,71 +396,13 @@ class Plot_Univariate(TruncationSelection,Plot_Summarized,Univariate):
 
     #             for g in pval.index.tolist():
     #     #             print(g,logkfda[g],logpval[g],c[g])
-    #                 ax.text(logkfda[g],logpval[g],g,color=c[g])
+    #                 ax.text(logkfda[g],logpval[g],g)#,color=c[g])
     #                 ax.set_xlim(xlim)
     #                 ax.set_ylim(0,logpval.max()*1.1)
     #                 genes += [g]
 
 
     #     return(genes,fig,ax)
-
-    def volcano_plot(self,var_prefix,color=None,exceptions=[],focus=None,zero_pvals=False,fig=None,ax=None,BH=False,threshold=1):
-        # quand la stat est trop grande, la fonction chi2 de scipy.stat renvoie une pval nulle
-        # on ne peut pas placer ces gènes dans le volcano plot alors ils ont leur propre graphe
-
-        if fig is None:
-            fig,ax = plt.subplots(figsize=(9,15))
-
-        BH_str = 'BHc' if BH else ''
-        zpval_str = '= 0' if zero_pvals else '>0'
-        dn = self.data_name
-
-        pval_name = f'{var_prefix}_pval{BH_str}' 
-        pval = filter_genes_wrt_pval(self.var[dn][pval_name],exceptions,focus,zero_pvals,threshold)
-        print(f'{var_prefix} ngenes with pvals {BH_str} {zpval_str}: {len(pval)}')
-        genes = []
-        if len(pval) != 0:
-            kfda = self.var[dn][f'{var_prefix}_kfda']
-            errB = self.var[dn][f'{var_prefix}_errB']
-            kfda = kfda[kfda.index.isin(pval.index)]
-            errB = errB[errB.index.isin(pval.index)]
-
-            logkfda = np.log(kfda)
-
-            xlim = (logkfda.min()-1,logkfda.max()+1)
-    #             c = self.color_volcano_plot(var_prefix,pval.index,color=color)
-
-            if zero_pvals:
-        #         print('zero')
-                ax.set_title(f'{var_prefix} \ng enes strongly rejected',fontsize=30)
-                ax.set_xlabel(f'log(kfda)',fontsize=20)
-                ax.set_ylabel(f'errB',fontsize=20)
-
-                for g in pval.index.tolist():
-        #             print(g,logkfda[g],errB[g],c[g])
-                    ax.text(logkfda[g],errB[g],g)#,color=c[g])
-                    ax.set_xlim(xlim)
-                    ax.set_ylim(0,1)
-                    genes += [g]
-
-
-            else:
-        #         print('nz')
-                ax.set_title(f'{var_prefix}\n non zero pvals',fontsize=30)
-                ax.set_xlabel(f'log(kfda)',fontsize=20)
-                ax.set_ylabel(f'-log(pval)',fontsize=20)
-                logpval = -np.log(pval)
-
-
-                for g in pval.index.tolist():
-        #             print(g,logkfda[g],logpval[g],c[g])
-                    ax.text(logkfda[g],logpval[g],g)#,color=c[g])
-                    ax.set_xlim(xlim)
-                    ax.set_ylim(0,logpval.max()*1.1)
-                    genes += [g]
-
-
-        return(genes,fig,ax)
 
 
     def volcano_plot_zero_pvals_and_non_zero_pvals(self,var_prefix,color_nz='errB',color_z='t',
