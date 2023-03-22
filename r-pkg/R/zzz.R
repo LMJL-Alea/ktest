e8a7686d67bfe8f9c9a53e15e24a975f781a648f
Ghislain Durif <gd.dev@libertymail.net>
2023-03-22 11:07:00 +0100

    restore onload function to use reticulate import delay functionality

--- r-pkg/R/zzz.R
+++ r-pkg/R/zzz.R
@@ -2,8 +2,10 @@
 pyktest <- NULL
 
 .onLoad <- function(libname, pkgname) {
-    if(check_pyktest(warn = FALSE)) {
-        # use superassignment to update global reference to pyktest
-        pyktest <<- reticulate::import("ktest", delay_load = TRUE)
-    }
+    # if(check_pyktest(warn = FALSE)) {
+    #     # use superassignment to update global reference to pyktest
+    #     pyktest <<- reticulate::import("ktest", delay_load = TRUE)
+    # }
+    # use superassignment to update global reference to pyktest
+    pyktest <<- reticulate::import("ktest", delay_load = TRUE)
 }
