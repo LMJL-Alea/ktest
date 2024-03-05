356bb2860634ab610abeaf0245b1986412499a48
parsenteva <111500199+parsenteva@users.noreply.github.com>
2024-03-05 20:01:21 +0100

    Return to one dataset + metadata structure

--- python/ktest/data.py
+++ python/ktest/data.py
@@ -24,16 +24,16 @@ class Data():
     sample_names: 
 
     """  
-    def __init__(self, data_1, data_2, sample_names=None):
+    def __init__(self, data, metadata, sample_names=None):
         """
 
         Parameters
         ----------
-            data_1 : Pandas.DataFrame 
+            data : Pandas.DataFrame 
                 the data dataframe
             
-            data_2 : Pandas.DataFrame 
-                the data dataframe
+            metadata : Pandas.DataFrame 
+                the metadata dataframe
                 
             sample_names: 
                 
@@ -45,14 +45,28 @@ class Data():
         self.nobs = {}
         self.sample_names = sample_names if sample_names is not None else ['Sample 1',
                                                                            'Sample 2']
-        self.data[self.sample_names[0]] = data_1
-        self.data[self.sample_names[1]] = data_2
+        try:
+            assert len(data.squeeze().shape) <= 2, 'Data has to be at most 2-dimensional'
+            assert len(metadata.squeeze().shape) == 1, 'Metadata has to be 1-dimensional'
+            if not (hasattr(data, 'index') or hasattr(metadata, 'index')):
+                assrt_str_1 = 'If index is not provided, '
+                assrt_str_1 += 'data and metadata have to have the same first dimension'
+                assert data.shape[0] == data.shape, assrt_str_1
+            if isinstance(metadata, pd.DataFrame):
+                meta_fmt = metadata[metadata.columns[0]]
+            else:
+                meta_fmt = metadata.copy()
+            levels = meta_fmt.unique() if hasattr(meta_fmt, 'unique') else np.unique(metadata)
+            assrt_str_2 = 'Metadata required to contain exactly 2 levels for two-sample test'
+            assert len(levels) == 2, assrt_str_2
             
-        for n, data_n in self.data.items():
-            try:
-                assert len(data_n.shape) <= 2, 'Data has to be at most 2-dimensional'
+            self.sample_names = sample_names if sample_names is not None else levels
+            self.data[self.sample_names[0]] = data[meta_fmt == levels[0]]
+            self.data[self.sample_names[1]] = data[meta_fmt == levels[1]]
+            
+            for n, data_n in self.data.items():
                 self.nobs[n] = data_n.shape[0]
-                self.nvar[n] = data_n.shape[1] if len(data_n.shape) == 2 else 1
+                self.nvar[n] = data_n.shape[1] if len(data_n.squeeze().shape) == 2 else 1
                 self.index[n] = data_n.index if hasattr(data_n, 'index') else np.arange(self.nobs[n])
                 self.variables[n] = data_n.columns if hasattr(data_n, 'columns') else np.arange(self.var[n])
                 
@@ -66,9 +80,8 @@ class Data():
                 else:
                     X = data_n.to_numpy() if not isinstance(data_n, np.ndarray) else data_n.copy()
                     self.data[n] = torch.from_numpy(X).double()
-            except AttributeError:
-                print(f'unknown data type {type(data_n)}')
-                break
+        except AttributeError:
+            print(f'Unknown data type {type(data_n)}')
         self.ntot = sum(self.nobs.values())
 
    
