022ce149ee591f96ea73e634ea597bef6bc6e5d7
Ghislain Durif <gd.dev@libertymail.net>
2024-04-18 10:36:32 +0200

    update ktest package man page

--- r-pkg/R/ktest-package.R
+++ r-pkg/R/ktest-package.R
@@ -1,8 +1,23 @@
-#' @keywords internal
-#' @importFrom checkmate assert_character assert_choice assert_count 
-#' assert_data_frame assert_integerish assert_logical assert_string qassert
+#' @name ktest-package
+#' @title ktest
+#' 
+#' @description
+#' Kernel-based testing
+#' 
+#' @details
+#' `ktest` implements kernel-based statistical testing, especially 
+#' for differential expression analysis in transcriptomics data. See 
+#' Ozier-Lafontaine et al (2024) for more details. 
+#' 
+#' @author 
+#' - [Anthony Ozier-Lafontaine](https://www.researchgate.net/profile/Anthony-Ozier-Lafontaine)
+#' - [Polina Arsenteva](https://parsenteva.github.io/)
+#' - [Ghislain Durif](https://gdurif.perso.math.cnrs.fr/)
+#' 
+#' @references
+#' Anthony Ozier-Lafontaine, Camille Fourneaux, Ghislain Durif, Polina 
+#' Arsenteva, Céline Vallot, Olivier Gandrillon, Sandrine Gonin-Giraud, 
+#' Bertrand Michel, Franck Picard. (2024). Kernel-Based Testing for 
+#' Single-Cell Differential Analysis. Preprint, 
+#' [hal-04214858](https://hal.science/hal-04214858).
 "_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
-NULL
