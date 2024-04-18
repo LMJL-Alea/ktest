363fc3568cffb51f855c9ef25df01a185ae7c057
Ghislain Durif <gd.dev@libertymail.net>
2024-04-18 17:18:43 +0200

    minor: improve checks

--- r-pkg/tests/testthat/test-3_data.R
+++ r-pkg/tests/testthat/test-3_data.R
@@ -1,7 +1,7 @@
 test_that("load_example_data", {
     out <- load_example_data()
     checkmate::expect_list(out, len = 2)
-    checkmate::expect_subset(names(out), c("data_tab", "metadata_tab"))
+    checkmate::expect_set_equal(names(out), c("data_tab", "metadata_tab"))
     checkmate::expect_data_frame(
         out$data_tab, nrows = 344, ncols = 83, types = "numeric")
     checkmate::expect_data_frame(
