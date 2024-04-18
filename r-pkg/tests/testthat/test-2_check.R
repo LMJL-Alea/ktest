f7b0ff8c5f5f092129d680cad2b45aa616cb3e9f
Ghislain Durif <gd.dev@libertymail.net>
2024-04-18 10:46:15 +0200

    revamp check function tests

--- r-pkg/tests/testthat/test-2_check.R
+++ r-pkg/tests/testthat/test-2_check.R
@@ -1,24 +1,11 @@
-# FIXME
-# test_that("check_python", {
-#     skip_if_no_python()
-#     skip_if_not_interactive()
-#     expect_true(check_python())
-#     expect_warning(check_python(version = "100000"))
-#     expect_false(check_python(version = "100000", warn = FALSE))
-# })
-# 
-# test_that("check_pyktest", {
-#     skip_if_no_python()
-#     skip_if_not_interactive()
-#     
-#     run_py_env({
-#         cat(str(reticulate::py_discover_config()))
-#         expect_warning(check_pyktest())
-#         expect_false(check_pyktest(warn = FALSE))
-#     })
-#     
-#     run_py_env({
-#         install_pyktest()
-#         expect_true(check_pyktest())
-#     })
-# })
+test_that("check_python", {
+    checkmate::expect_flag(check_python(verbose = FALSE))
+    skip_if_no_python()
+    expect_true(expect_message(check_python()))
+    expect_false(expect_warning(check_python(version = "100000")))
+})
+
+test_that("check_ktest", {
+    skip_if_no_python()
+    checkmate::expect_flag(check_ktest(verbose = FALSE))
+})
